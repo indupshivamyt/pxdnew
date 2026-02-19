39f04e376991132a6bb11e9a283a0d46341d5212
indupshivamyt <ind.up.shivamyt@gmail.com>
2026-02-19 20:59:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const navLinks = document.getElementById("navLinks");
 menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+
+/* Bottom nav active toggle */
+const navItems = document.querySelectorAll(".nav-item");
+
+navItems.forEach(item => {
+    item.addEventListener("click", () => {
+        navItems.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
+    });
+});
